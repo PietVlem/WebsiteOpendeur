9fc47ee20e715708346032990b55764b91e7c27e
Pieter Vleminckx <pieter.vlem@gmail.com>
2018-03-02 15:44:38 +0100

    font update

--- opendeurReact/src/App.js
+++ opendeurReact/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+//import components
 import ComingSoon from './components/comingSoon/comingSoon.js';
+
 
 class App extends Component {
   render() {
@@ -12,3 +14,4 @@ class App extends Component {
 }
 
 export default App;
+
--- opendeurReact/src/components/comingSoon/comingSoon.js
+++ opendeurReact/src/components/comingSoon/comingSoon.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import ReactFontFace from 'react-font-face'
+
+//import style
+import NeoSansProBold from '../../fonts/NeoSansPro-Bold.otf';
+import NeoSansProMed from '../../fonts/NeoSansPro-Medium.otf';
 import './comingSoon.scss';
 
 // images
-import banner from './banner.svg';
+import banner from './banner.png';
+
+const fonts = {
+  main: {
+    fontFamily: 'NeoSansPro Bold',
+  }
+}
 
 class ComingSoon extends Component {
   render() {
     return (
       <div className="backGround">
         <div className="center">
-          <h1>Coming soon ...</h1>
+          <h1 style={fonts.main}>Coming soon ...</h1>
           <img src={banner} alt="openDeurBanner" />
         </div>
       </div>
@@ -17,4 +28,19 @@ class ComingSoon extends Component {
   }
 }
 
-export default ComingSoon;
+let fontConfig = {
+  google: [
+  ],
+  file: [
+    {
+      fontFamily: 'NeoSansPro Bold',
+      fontStyle:  'bold',
+      fontWeight: 700,
+      file: NeoSansProBold,
+      fontType: 'opentype',
+      fileLocal: 'NeoSansPro-Bold'
+    },
+  ],
+}
+
+export default ReactFontFace(ComingSoon, fontConfig);
